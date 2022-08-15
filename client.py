687e548f09f3fc93913ca9c047a7b494584695be
Albiorin <dorukyldrm006@gmail.com>
2022-08-15 14:00:07 +0300

    i fucked up the code lmao

--- client.py
+++ client.py
@@ -4,34 +4,86 @@ import pickle
 import struct
 import time
 
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import cv2
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Welcome to Helios Ground Systems")
-time.sleep(2)
-host = input('Enter instrument ip address: ')
 time.sleep(1)
+host = input('Enter instrument ip address: ')
 print('Connecting...')
-time.sleep(2)
 port = 80
 client_socket.connect((host, 80))
 data = b""
 
 payload_size = struct.calcsize("Q")
 
-while True:
-    while len(data) < payload_size:
-        packet = client_socket.recv(4*1024)
-        if not packet: break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += client_socket.recv(4*1024)
-    frame_data = data[:msg_size] 
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("Client", frame)
-    key = cv2.waitKey(10)
-    if key == 13:
-        break
-client_socket.close()
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.VBL = QVBoxLayout()
+
+        self.FeedLabel = QLabel()
+        self.VBL.addWidget(self.FeedLabel)
+
+        self.CancelBTN = QPushButton("Cancel")
+        self.CancelBTN.clicked.connect(self.CancelFeed)
+        self.VBL.addWidget(self.CancelBTN)
+
+        self.Worker1 = Worker1()
+
+        self.Worker1.start()
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+        self.setLayout(self.VBL)
+
+    def ImageUpdateSlot(self, Image):
+        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
+
+    def CancelFeed(self):
+        self.Worker1.stop()
+
+class Worker1(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+    def run(self):
+        self.ThreadActive = True
+        Capture = cv2.VideoCapture(0)
+        while len(data) < payload_size:
+            packet = client_socket.recv(4*1024)
+            if not packet: break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += client_socket.recv(4*1024)
+        frame_data = data[:msg_size] 
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        #cv2.imshow('Output', frame)
+        #key = cv2.waitKey(10)
+        #if key == 13:
+            #break
+        #client_socket.close()
+        while self.ThreadActive:
+            ret, frame = Capture.read()
+            if ret:
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImage = cv2.flip(Image, 1)
+                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(Pic)
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    Root = MainWindow()
+    Root.show()
+    sys.exit(App.exec())
