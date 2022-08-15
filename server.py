04c08108a6971318c4ffc46608127ec80a50e9ca
Albiorin <dorukyldrm006@gmail.com>
2022-08-15 14:04:48 +0300

    qt done but no vid >:(

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import cv2
 import pickle
 import struct
 
-host = '169.254.42.82'
+host = '10.20.50.4'
 port = 80
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,7 +40,6 @@ while True:
             a = pickle.dumps(frame)
             message = struct.pack("Q", len(a))+a
             client_socket.sendall(message)
-            cv2.imshow('Server',frame)
             key = cv2.waitKey(10)
             if key == 13:
                 client_socket.close()
